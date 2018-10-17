4a6362db6ce31f13cecc8c7cf1c225c0ec3a796d
trung thuc <34118032+fdssssf@users.noreply.github.com>
2018-10-17 19:38:45 +0700

    init project

---
+++ src/main/java/com/group4/ProjectChototApplication.java
@@ -0,0 +1,12 @@
+package com.group4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectChototApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectChototApplication.class, args);
+	}
+}
