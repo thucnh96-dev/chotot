512eab3aa6f5cc6f994a9aed3be02d5b9dfe12c1
tranquoctuyen97 <tuyentqpd02042@fpt.edu.vn>
2018-11-03 11:23:20 +0700

    update name file

--- src/main/java/com/group4/Controller/UserController.java
+++ src/main/java/com/group4/Controller/UserController.java
@@ -5,7 +5,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class testControllerJSP {
+public class UserController {
 @GetMapping(value="/login")
 	public String honme(ModelMap mm) {
 	mm.addAttribute("meet", "trungthuc");
