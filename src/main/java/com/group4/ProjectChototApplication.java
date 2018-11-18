8c1620b5099ad1e07f97a436f9db34da305b71d8
thucnhpd02000@fpt.edu.vn <thucnhpd02000@fpt.edu.vn>
2018-11-18 15:30:55 +0700

    upload

--- src/main/java/com/group4/ProjectChototApplication.java
+++ src/main/java/com/group4/ProjectChototApplication.java
@@ -3,9 +3,15 @@ package com.group4;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import com.group4.Heper.FileStorageProperties;
+
 @SpringBootApplication
+@EnableConfigurationProperties({
+    FileStorageProperties.class
+})
 public class ProjectChototApplication  extends SpringBootServletInitializer {
 	 @Override
 	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
